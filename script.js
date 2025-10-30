872bb6b4ce3afaf22c2cd81d4435c08d1a076d83
AmalieKoder <amlie.koder@gmail.com>
2025-10-30 10:39:47 +0100

    amalie edit

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 
-/* Example of a chart
-const ctx = document.getElementById('myChart');
+/*const ctx = document.getElementById('myChart');
 
 new Chart(ctx, {
-type: 'bar',
-data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-    label: '# of Votes',
-    data: [12, 19, 3, 5, 2, 3],
-    borderWidth: 1
-    }]
-},
-options: {
-    scales: {
-    y: {
-        beginAtZero: true
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
     }
+});*/
+
+
+document.getElementById('dark-mode').addEventListener('input', () => {
+    if (Boolean(document.getElementById('dark-mode').value)) {
+        document.body.classList.add('dark-mode')
+    } else{
+        document.body.classList.remove('dark-mode')
     }
-}
-});
-*/
+})
